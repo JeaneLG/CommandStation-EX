ac62d62e3d1b8957d47314c93712bf02a04b9725
Jeanette Schlosser <JeaneLG@vodafonemail.de>
2022-11-18 02:24:06 +0100

    Added HAL for S88 Master driven directly by the arduino. For a small layout you can attach the S88 bus directly to the selected arduino pins and power supply.

---
+++ S88.cpp
@@ -0,0 +1,98 @@
+#include "S88.h"
+#include "Sensors.h"
+
+S88Master::S88Master(byte dataPin, byte clkPin, byte psPin, byte resetPin, byte numModules, int firstVpin) : (firstVpin, InputsPerModule*numModules)
+{
+  s88Module = numModules;
+  data = new byte[s88Module];
+  s88sendon = '0';
+  s88RCount = 0;
+  s88RMCount = 0;
+  s88DataPin = dataPin;
+  s88ClkPin = clkPin;
+  s88PsPin = psPin;
+  s88ResetPin = resetPin;
+  
+  // add S88Master to list of devices
+  addDevice(this);
+}
+
+void S88Master::_begin()
+{
+  pinMode(s88ResetPin, OUTPUT);    //Reset
+  pinMode(s88PsPin, OUTPUT);      //PS/LOAD
+  pinMode(s88ClkPin, OUTPUT);      //Clock
+  digitalWrite(s88ResetPin, LOW);
+  digitalWrite(s88PsPin, LOW);      //init
+  digitalWrite(s88ClkPin, LOW);
+  
+  pinMode(s88DataPin, INPUT);    //Dateneingang
+
+  createSensors();
+  _deviceState = DEVSTATE_NORMAL;
+}
+
+void S88Master::_loop(unsigned long currentMicros)
+{
+  if (s88RCount == 3)    //Load/PS Leitung auf 1, darauf folgt ein Schiebetakt nach 10 ticks!
+    digitalWrite(s88PsPin, HIGH);
+  if (s88RCount == 4)   //Schiebetakt nach 5 ticks und S88Module > 0
+    digitalWrite(s88ClkPin, HIGH);       //1. Impuls
+  if (s88RCount == 5)   //Read Data IN 1. Bit und S88Module > 0
+    s88readData();    //LOW-Flanke während Load/PS Schiebetakt, dann liegen die Daten an
+  if (s88RCount == 9)    //Reset-Plus, löscht die den Paralleleingängen vorgeschaltetetn Latches
+    digitalWrite(s88ResetPin, HIGH);
+  if (s88RCount == 10)    //Ende Resetimpuls
+    digitalWrite(s88ResetPin, LOW);
+  if (s88RCount == 11)    //Ende PS Phase
+    digitalWrite(s88PsPin, LOW);
+  if (s88RCount >= 12 && s88RCount < 10 + (s88Module * 8) * 2) {    //Auslesen mit weiteren Schiebetakt der Latches links
+    if (s88RCount % 2 == 0)      //wechselnder Taktimpuls/Schiebetakt
+      digitalWrite(s88ClkPin, HIGH);  
+    else s88readData();    //Read Data IN 2. bis (Module*8) Bit
+  }
+  s88RCount++;      //Zähler für Durchläufe/Takt
+  if (s88RCount >= 10 + (s88Module * 8) * 2) {  //Alle Module ausgelesen?
+    s88RCount = 0;                    //setzte Zähler zurück
+    s88RMCount = 0;                  //beginne beim ersten Modul von neuem
+    //init der Grundpegel
+    digitalWrite(s88PsPin, LOW);    
+    digitalWrite(s88ClkPin, LOW);
+    digitalWrite(s88ResetPin, LOW);
+    if (s88sendon == 's')  //Änderung erkannt
+      s88sendon = 'i';      //senden
+  }
+  
+  delayUntil(LOOP_STEP_TIME_MICROS + currentMicros); // set time when this method is called next
+}
+
+int S88Master::_read(VPIN vpin)
+{
+  VPIN absPin = vpin - _firstVpin;
+  byte Modul = absPin / InputsPerModule;
+  byte Port = absPin % InputsPerModule;
+  return bitRead(data[Modul],Port);
+}
+
+void S88Master::_display() {
+  DIAG(F("S88Master Configured on VPins:%d-%d"), (int)_firstVpin, (int)_firstVpin+_nPins-1);
+}
+
+void S88Master::createSensors()
+{
+  int pin = FIRST_VPIN_S88;
+  for (; pin<FIRST_VPIN_S88+_nPins; pin++) Sensor::create(pin, pin, 1);
+}
+
+//Einlesen des Daten-Bit und Vergleich mit vorherigem Durchlauf
+void S88Master::s88readData() {
+  digitalWrite(s88ClkPin, LOW);  //LOW-Flanke, dann liegen die Daten an
+  byte Modul = s88RMCount / 8;
+  byte Port = s88RMCount % 8;
+  byte getData = digitalRead(s88DataPin);  //Bit einlesen
+  if (bitRead(data[Modul],Port) != getData) {     //Zustandsänderung Prüfen?
+    bitWrite(data[Modul],Port,getData);          //Bitzustand Speichern
+    s88sendon = 's';  //Änderung vorgenommen. (SET)
+  }
+  s88RMCount++;
+}
---
+++ S88.h
@@ -0,0 +1,74 @@
+/**
+ * S88 Master.
+ */
+#ifndef S88_h
+#define S88_h
+
+#include <Arduino.h>
+
+#include "IODevice.h"
+
+// Defines the frequency of the S88 Bus
+#define LOOP_STEP_TIME_MICROS 4 // 250kHz
+
+//--------------------------------------------------------------
+//Pinbelegungen am Dekoder:
+  //Eingänge:
+#define S88DataPin 4      //S88 Data IN
+  //Ausgänge:
+#define S88ClkPin 5    //S88 Clock
+#define S88PSPin 6    //S88 PS/LOAD
+#define S88ResetPin 7    //S88 Reset
+
+#define FIRST_VPIN_S88 880
+#define InputsPerModule 8
+
+class S88Master;
+static S88Master *first = NULL;
+
+class S88Master : public IODevice
+{
+  public:
+    static void create(byte dataPin=S88DataPin, byte clkPin=S88ClkPin, byte psPin=S88PSPin, byte resetPin=S88ResetPin, byte numModules=3, int firstVpin=FIRST_VPIN_S88) {
+      S88Master *next = first;
+      first = new S88Master(dataPin, clkPin, psPin, resetPin, numModules, firstVpin);
+      first->next = next;
+    }
+
+  protected:
+    void _begin() override;
+    void _loop(unsigned long currentMicros) override;
+    // supports only read operations
+    void _write(VPIN vpin, int value) override {};
+    int _read(VPIN vpin) override;
+    void _display() override;
+
+  private:
+    S88Master *next = NULL;
+    byte s88Module;    //Anzahl der 8x Module
+                       //maximal 62 x 8 Port Module = 31 Module à 16 Ports
+    byte *data;        //Zustandsspeicher
+    /*
+    '0' = keine
+    's' = Änderungen vorhanden, noch nicht fertig mit Auslesen
+    'i' = Daten vollständig, senden an PC
+    */
+    char s88sendon;       //Bit Änderung
+    
+    int s88RCount = 0;    //Lesezähler 0-39 Zyklen
+    int s88RMCount = 0;   //Lesezähler Modul-Pin
+
+    byte s88DataPin;
+    byte s88ClkPin;
+    byte s88PsPin;
+    byte s88ResetPin;
+    
+    S88Master(byte dataPin, byte clkPin, byte psPin, byte resetPin, byte numModules, int firstVpin);
+    /**
+     * Creates a sensor for each S88 module input.
+     */
+    void createSensors();
+    void s88readData();
+};
+
+#endif
